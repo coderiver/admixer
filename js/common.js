aaa5577578a757e9a0768a25bcb7bf906f60d1ea
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-06-30 00:59:05 +0300

    admarket

--- js/common.js
+++ js/common.js
@@ -15,42 +15,6 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	function bla(){
-    	var observe;
-    	if (window.attachEvent) {
-    	    observe = function (element, event, handler) {
-    	        element.attachEvent('on'+event, handler);
-    	    };
-    	}
-    	else {
-    	    observe = function (element, event, handler) {
-    	        element.addEventListener(event, handler, false);
-    	    };
-    	}
-    	function textareaResize() {
-    	    var text = document.getElementById('text');
-    	    function resize () {
-    	        text.style.height = 'auto';
-    	        text.style.height = text.scrollHeight+'px';
-    	    }
-    	    /* 0-timeout to get the already changed text */
-    	    function delayedResize () {
-    	        window.setTimeout(resize, 0);
-    	    }
-    	    observe(text, 'change',  resize);
-    	    observe(text, 'cut',     delayedResize);
-    	    observe(text, 'paste',   delayedResize);
-    	    observe(text, 'drop',    delayedResize);
-    	    observe(text, 'keydown', delayedResize);
-
-    	    //text.focus();
-    	    // text.select();
-    	    resize();
-    	};
-    	textareaResize();
-    }
-	bla();
-
 	var fpage = $('.desktop .js-fullpage');
 
 	function initFullPage() {
@@ -169,6 +133,38 @@ head.ready(function() {
     });
 
     
-    
+    function initRes(){
+    	var observe;
+    	if (window.attachEvent) {
+    	    observe = function (element, event, handler) {
+    	        element.attachEvent('on'+event, handler);
+    	    };
+    	}
+    	else {
+    	    observe = function (element, event, handler) {
+    	        element.addEventListener(event, handler, false);
+    	    };
+    	}
+    	function textareaResize() {
+    	    var text = document.getElementById('text');
+    	    function resize () {
+    	        text.style.height = 'auto';
+    	        text.style.height = text.scrollHeight+'px';
+    	    }
+    	    /* 0-timeout to get the already changed text */
+    	    function delayedResize () {
+    	        window.setTimeout(resize, 0);
+    	    }
+    	    observe(text, 'change',  resize);
+    	    observe(text, 'cut',     delayedResize);
+    	    observe(text, 'paste',   delayedResize);
+    	    observe(text, 'drop',    delayedResize);
+    	    observe(text, 'keydown', delayedResize);
+
+    	    resize();
+    	};
+    	textareaResize();
+    }
+	initRes();
 
 });
